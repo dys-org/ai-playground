2ad7690480b71c2736d45338085ca11a27a1123e
David Soards <david@soards.me>
2024-11-30 00:23:13 -0500

    change A.I. font

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="mx-auto max-w-3xl p-4 pb-24 text-center">
     <div class="mt-24">
-      <div class="mb-8 text-4xl font-extralight">Welcome to my</div>
+      <div class="mb-8 text-4xl font-extralight">Welcome to the</div>
       <h1 class="sr-only">A.I. Playground</h1>
       <img src="@/assets/img/ai-playground.svg" alt="A.I. Playground" class="mx-auto w-96" />
     </div>
